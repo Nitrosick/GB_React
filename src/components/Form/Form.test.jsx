f6d04428dd4502434032c4bbc9b765aa5dacd426
Nikita <nitrosick@gmail.com>
2022-06-03 15:03:34 +0200

    Lesson_10 / Jest testing
    Lesson_10 / Jest testing

---
+++ src/components/Form/Form.test.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import { Form } from './Form';
+
+describe('Form', () => {
+    beforeEach(() => {
+        // const send = jest.fn();
+        // render(<Form sendMessage={send} />);
+    });
+
+    it('Check text in button', () => {
+        render(<Form />);
+
+        expect(screen.getByRole('button')).toBeTruthy();
+        expect(screen.getByText(/Send/)).toBeInTheDocument();
+    });
+
+    it('Button is enabled', () => {
+        render(<Form />);
+
+        expect(screen.getByRole('button')).toBeEnabled();
+    });
+
+    it('Button click with userEvent', async () => {
+        const send = jest.fn();
+        render(<Form sendMessage={send} />);
+        await userEvent.click(screen.getByRole('button'));
+
+        expect(send).toHaveBeenCalledTimes(1);
+    });
+
+    it('Test input', async () => {
+        const send = jest.fn();
+        const dom = render(<Form sendMessage={send} />);
+        await userEvent.type(dom.container.querySelector('#message_input'), 'Test message');
+
+        expect(dom.container.querySelector('#message_input').value).toBe('Test message');
+    });
+});

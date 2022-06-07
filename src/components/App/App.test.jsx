8b44727cfab99a9629125e04811f9ec398f545d6
Nikita <nitrosick@gmail.com>
2022-06-07 10:00:21 +0200

    Lesson_10 / Fixes
    Lesson_10 / Fixes

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,30 +1,36 @@
-// import React from 'react';
+import React from 'react';
 // import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
-// import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
 
 // import { Message } from '../Message/Message';
-// import { Form } from '../Form/Form';
+import { Form } from '../Form/Form';
 
-// describe('App', () => {
-//     it('Bot`s response', async () => {
-//         const send = jest.fn();
-//         render(<>
-//             <Message messageList={[]} />
-//             <Form sendMessage={send} />
-//         </>);
+describe('App', () => {
+  it('App render test', () => {
+    render(<Form />);
+    expect(true).toBeTruthy();
+  });
 
-//         fireEvent.input(screen.getByPlaceholderText('Your message...'), {
-//             target: { value: 'Test message' },
-//         });
-//         fireEvent.click(screen.getByRole('button'));
+  // it('Bot`s response', async () => {
+  //     const send = jest.fn();
+  //     render(<>
+  //         <Message messageList={[]} />
+  //         <Form sendMessage={send} />
+  //     </>);
 
-//         //Вариант 1
-//         expect(await screen.findByText('Robot')).toBeInTheDocument();
+  //     fireEvent.input(screen.getByPlaceholderText('Your message...'), {
+  //         target: { value: 'Test message' },
+  //     });
+  //     fireEvent.click(screen.getByRole('button'));
 
-//         //Вариант 2
-//         await waitFor(() =>
-//             expect(screen.getByText('Robot')).toBeInTheDocument()
-//         );
-//     });
-// });
+  //     //Вариант 1
+  //     expect(await screen.findByText('Robot')).toBeInTheDocument();
+
+  //     //Вариант 2
+  //     await waitFor(() =>
+  //         expect(screen.getByText('Robot')).toBeInTheDocument()
+  //     );
+  // });
+});
